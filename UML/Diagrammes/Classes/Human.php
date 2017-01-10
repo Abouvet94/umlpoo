0e19de4cddadbb7b1a4ae2e1cc5a6ac6eaa533f1
Vincent Letourneau <vincent@nanoninja.com>
2017-01-10 10:03:52 +0100

    Ajout de la classe Human

---
+++ UML/Diagrammes/Classes/Human.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Description of Human
+ *
+ * @file Human.php
+ * @author dev
+ */
+class Human
+{
+    private $height = 0;
+    private $weight = 0;
+    private $hairColor = "";
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    public function getHairColor()
+    {
+        return $this->hairColor;
+    }
+
+    public function setHeight($height)
+    {
+        $this->height = (int) $height;
+        return $this;
+    }
+
+    public function setWeight($weight)
+    {
+        $this->weight = (int) $weight;
+        return $this;
+    }
+
+    public function setHairColor($hairColor)
+    {
+        $this->hairColor = (string) $hairColor;
+        return $this;
+    }
+}
